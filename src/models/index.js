17772966c48a18d35dd669fd400561bd85789662
mohamedabdelraouf <mohamedabdelraouf>
2025-06-17 05:31:57 +0300

    make the schema of DB

---
+++ src/models/index.js
@@ -0,0 +1,15 @@
+const User = require('./User');
+const Category = require('./Category');
+const Product = require('./Product');
+const Order = require('./Order');
+const Review = require('./Review');
+const Cart = require('./Cart');
+
+module.exports = {
+  User,
+  Category,
+  Product,
+  Order,
+  Review,
+  Cart
+};
---
+++ src/routes/cartRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+// Import controllers later
+
+// Routes will be implemented later
+router.route('/').get((req, res) => {
+  res.status(200).json({ message: 'Get cart' });
+});
+
+module.exports = router;
