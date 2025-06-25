eea98345c62daeff795f437d5940eef64de648a3
mohamedabdelraouf <mohamedabdelraouf>
2025-06-25 13:32:54 +0300

    make the user endpoint

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const router = express.Router();
-// Import controllers later
-// const { getUsers, getUser, createUser, updateUser, deleteUser } = require('../controllers/userController');
+const { protect } = require('../middleware/authMiddleware');
+const userController = require('../controllers/userController');
 
-// Routes will be implemented later
-router.route('/').get((req, res) => {
-  res.status(200).json({ message: 'Get all users' });
-});
+router.get('/me', protect, userController.getUser);
 
 module.exports = router;
