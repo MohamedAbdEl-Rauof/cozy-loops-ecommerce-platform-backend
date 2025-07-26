061bfa6cedb8b2d4ef2ca424a33758213748bead
mohamedabdelraouf <mohamedabdelraouf>
2025-07-26 10:49:25 +0300

    update the payment

--- index.js
+++ index.js
@@ -18,11 +18,6 @@ const server = http.createServer(app);
 
 initSocket(server);
 
-const webhookRoutes = require('./src/routes/webhookPayment');
-
-app.use('/webhook', webhookRoutes);
-
-
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
