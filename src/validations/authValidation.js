87594fef805c6c6968a237207063d27038984243
mohamedabdelraouf <mohamedabdelraouf>
2025-08-19 16:02:19 +0300

    enhance email temp

--- src/validations/authValidation.js
+++ src/validations/authValidation.js
@@ -10,14 +10,14 @@ const registerValidation = [
     .withMessage('First name is required')
     .isLength({ min: 2, max: 50 })
     .withMessage('First name must be between 2 and 50 characters'),
-  
+
   body('lastName')
     .trim()
     .notEmpty()
     .withMessage('Last name is required')
     .isLength({ min: 2, max: 50 })
     .withMessage('Last name must be between 2 and 50 characters'),
-  
+
   body('email')
     .trim()
     .notEmpty()
@@ -25,15 +25,15 @@ const registerValidation = [
     .isEmail()
     .withMessage('Please provide a valid email')
     .normalizeEmail(),
-  
-  body('password')
-    .trim()
-    .notEmpty()
-    .withMessage('Password is required')
-    .isLength({ min: 8 })
-    .withMessage('Password must be at least 8 characters')
-    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]/)
-    .withMessage('Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character')
+
+body('password')
+  .trim()
+  .notEmpty()
+  .withMessage('Password is required')
+  .isLength({ min: 8 })
+  .withMessage('Password must be at least 8 characters')
+  .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&_()#^])[A-Za-z\d@$!%*?&_()#^]+$/)
+  .withMessage('Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character')
 ];
 
 /**
@@ -47,7 +47,7 @@ const loginValidation = [
     .isEmail()
     .withMessage('Please provide a valid email')
     .normalizeEmail(),
-  
+
   body('password')
     .trim()
     .notEmpty()
@@ -62,17 +62,17 @@ const resetPasswordValidation = [
     .notEmpty().withMessage('Password is required')
     .isLength({ min: 8 }).withMessage('Password must be at least 8 characters')
     .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*])/).withMessage('Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character'),
-  
+
 ];
 
 const otpVaildation = [
   body('email')
-  .isEmail()
-  .withMessage('Please provide a valid email'),
+    .isEmail()
+    .withMessage('Please provide a valid email'),
   body('otp')
-  .isLength({ min: 6, max: 6 })
-  .isNumeric()
-  .withMessage('OTP must be a 6-digit number'),
+    .isLength({ min: 6, max: 6 })
+    .isNumeric()
+    .withMessage('OTP must be a 6-digit number'),
 ];
 
 
